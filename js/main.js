fe7bc8b7ac38066a81f2e4ba34bc2f140b04b97e
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-10-26 13:32:58 +1100

    add recipe adapters

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@
         menu = menu + addmenubutton("2. Machine format", "01_machine_format.html")
         menu = menu + addmenubutton("3. Custom Machines: Variables", "02_variables.html")
         menu = menu + addmenubutton("4. Custom Machines: Machine Recipes", "03_machine_recipes.html")
+        menu = menu + addmenubutton("5. Custom Machines: Recipe Adapters", "04_recipe_adapters.html")
         menu = menu + "</ul>"
         menuelement.innerHTML = menu
     }
