e02b0ed1ef14082fc22c38c36a262bbdd60dba96
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-10-26 15:31:19 +1100

    add furnace fuel as item input

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
     function genmenu() {
         let menuelement = document.getElementById("menu-div")
         let menu = "<ul class='menu'>"
-        menu = menu + addmenubutton("home", "index.html")
+        menu = menu + addmenubutton("Home", "index.html")
         menu = menu + addmenubutton("1. Keynotes", "00_keynotes.html")
         menu = menu + addmenubutton("2. Machine format", "01_machine_format.html")
         menu = menu + addmenubutton("3. Custom Machines: Variables", "02_variables.html")
@@ -19,6 +19,7 @@
         menu = menu + addmenubutton("8. Custom Machines: Machine Format Position Permutations [Advanced]", "07_machine_format_postition_permutations.html")
         menu = menu + addmenubutton("9. Custom Machines: Machine Colors", "08_machine_colors.html")
         menu = menu + addmenubutton("10. Custom Machines: Structure To JSON Tool [Advanced]", "09_structure_to_json_tool.html")
+        menu = menu + addmenubutton("11. Custom Machines: Furnace Fuel as Item Input", "010_furnace_fuel_as_item_input.html")
         menu = menu + "</ul>"
         menuelement.innerHTML = menu
     }
