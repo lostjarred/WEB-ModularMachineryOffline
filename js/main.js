23f6730c9e2b5a9067cb61b701ee80ef08ec08fb
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-10-26 15:38:05 +1100

    add craftweaker recipe definations

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@
         menu = menu + addmenubutton("9. Custom Machines: Machine Colors", "08_machine_colors.html")
         menu = menu + addmenubutton("10. Custom Machines: Structure To JSON Tool [Advanced]", "09_structure_to_json_tool.html")
         menu = menu + addmenubutton("11. Custom Machines: Furnace Fuel as Item Input", "010_furnace_fuel_as_item_input.html")
+        menu = menu + addmenubutton("12. Crafttweaker Recipe definitions [Advanced]", "011_craftweaker_recipe_definitions.html")
         menu = menu + "</ul>"
         menuelement.innerHTML = menu
     }
